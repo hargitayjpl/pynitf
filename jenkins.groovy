19302e464164fe34de43e0b52eff52c4be1de2e1
Brandon Hargitay <bhargitay@localhost.localdomain>
2023-08-07 16:02:40 -0700

    add jenkins file

---
+++ jenkins.groovy
@@ -0,0 +1,37 @@
+pipeline {
+    agent { 
+        docker { 
+            image 'artifactory.jpl.nasa.gov:16003/gov/nasa/jpl/island/imd/centos7-videoapp-env:latest'
+            registryUrl 'https://artifactory.jpl.nasa.gov:16003'
+            registryCredentialsId  'artifactory_credential_id'
+            args '--user 0:0'
+            reuseNode true
+        } 
+    }
+    stages {
+        stage('Run Unit Tests') {
+            steps {
+                sh 'pytest tests/'
+            }
+        }
+    }
+    post {
+        failure {
+            emailext (
+                to: 'hargitay@jpl.nasa.gov',
+                subject: 'Build FAILURE in Jenkins: $PROJECT_NAME - #$BUILD_NUMBER',
+                body: 'Check console output at $BUILD_URL to view the results. Build has failed.'
+            )
+        }
+        success {
+            emailext (
+                to: 'hargitay@jpl.nasa.gov',
+                subject: 'Build SUCCESS in Jenkins: $PROJECT_NAME - #$BUILD_NUMBER',
+                body: 'Check console output at $BUILD_URL to view the results. Build was successful.'
+            )
+        }
+        cleanup {
+            cleanWs()
+        }
+    }
+}
